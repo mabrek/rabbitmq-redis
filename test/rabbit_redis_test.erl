05ad11088a74f01d7fb4f2d26961ff685ee17625
Anton Lebedevich <mabrek@gmail.com>
2011-02-21 18:20:35 +0300

    failing redis test

--- test/rabbit_redis_test.erl
+++ test/rabbit_redis_test.erl
@@ -3,4 +3,17 @@
 
 test() ->
     ok = application:start(rabbit_redis),
-    ok = application:stop(rabbit_redis).
+    ok = application:stop(rabbit_redis),
+    
+    Redis = erldis_sup:client(),
+    RedisChannel = <<"channel">>,
+    Payload = <<"payload">>,
+    erldis:subscribe(Redis, RedisChannel, self()),
+    erldis:publish(Redis, RedisChannel, Payload),
+    receive
+        {message, Payload, Channel} -> 
+            ok
+    after
+        100 ->
+             throw(timeout)
+    end.
