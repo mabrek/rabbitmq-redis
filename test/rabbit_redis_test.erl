b20bf08ee057f8ec8a94701f8eee9104d9868ae5
Anton Lebedevich <mabrek@gmail.com>
2011-02-22 14:58:05 +0300

    separate clients for pub/sub, cosmetics

--- test/rabbit_redis_test.erl
+++ test/rabbit_redis_test.erl
@@ -1,16 +1,24 @@
 -module(rabbit_redis_test).
 -export([test/0]).
 
+-define(REDIS_HOST, "localhost").
+-define(REDIS_PORT, 6379).
+
 test() ->
+    start_stop(),
+    redis_only_pubsub().
+
+start_stop() ->
     ok = application:start(rabbit_redis),
-    ok = application:stop(rabbit_redis),
-    
-    {ok, RedisPublisher} = erldis:connect(),
-    Redis = erldis_sup:client(),
-    RedisChannel = <<"channel">>,
+    ok = application:stop(rabbit_redis).
+
+redis_only_pubsub() ->
+    {ok, Publisher} = erldis_client:start_link(?REDIS_HOST, ?REDIS_PORT),
+    {ok, Subscriber} = erldis_client:start_link(?REDIS_HOST, ?REDIS_PORT),
+    Channel = <<"channel">>,
     Payload = <<"payload">>,
-    erldis:subscribe(Redis, RedisChannel, self()),
-    1 = erldis:publish(RedisPublisher, RedisChannel, Payload),
+    erldis:subscribe(Subscriber, Channel, self()),
+    1 = erldis:publish(Publisher, Channel, Payload),
     receive
         {message, Channel, Payload} -> 
             ok;
