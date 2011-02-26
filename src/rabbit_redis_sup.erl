5318752a7f121c53972254f59c5f257f0fc3a1c1
Anton Lebedevich <mabrek@gmail.com>
2011-02-26 13:29:44 +0300

    cosmetics

--- src/rabbit_redis_sup.erl
+++ src/rabbit_redis_sup.erl
@@ -9,22 +9,17 @@
 -export([init/1]).
 
 start_link(Bridges) ->
-    % TODO empty bridges
-    supervisor2:start_link({local, ?MODULE}, ?MODULE, child_specs(Bridges)).
+    supervisor2:start_link(
+      {local, ?MODULE}, 
+      ?MODULE, 
+      [{rabbit_redis_subscribe,
+        {rabbit_redis_subscribe, start_link, [Config]},
+        transient,
+        16#ffffffff,
+        worker,
+        [rabbit_redis_subscribe]
+       } || Config <- Bridges]).
 
 init(Childs) ->
     % TODO delayed restart
     {ok, {{one_for_one, 10, 10}, Childs}}.
-
-child_specs(Bridges) ->
-    lists:map(fun child_spec/1, Bridges).
-
-child_spec(Config) ->
-    % TODO use type from config to determine module
-    {rabbit_redis_subscribe,
-     {rabbit_redis_subscribe, start_link, [Config]},
-     transient,
-     16#ffffffff,
-     worker,
-     [rabbit_redis_subscribe]
-    }.
